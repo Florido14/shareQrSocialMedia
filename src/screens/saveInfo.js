45ebd59abf4b5b7507b92592014a81bbf12d530a
Florido14 <51244465+Florido14@users.noreply.github.com>
2021-11-12 20:00:16 -0600

    SaveInfo interface finished, only functionality is missing

--- src/screens/saveInfo.js
+++ src/screens/saveInfo.js
@@ -12,6 +12,15 @@ import instaicon from "../assets/img/instagram.png";
 import twiticon from "../assets/img/twitter.png";
 import snapicon from "../assets/img/snapchat.png";
 import tikicon from "../assets/img/tiktok.png";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+//falta setear en el state para luego guardarlo en localstorage y generar el codigo QR
 
 const Item = styled(Paper)(({ theme }) => ({
   ...theme.typography.body2,
@@ -19,64 +28,291 @@ const Item = styled(Paper)(({ theme }) => ({
   textAlign: "center",
   color: "transparent",
   backgroundColor: "transparent",
-  borderRadius: "500px",
+  borderRadius: "none",
   boxShadow: "none",
   paddingTop: "30px",
 }));
+
+function FormDialog() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div className="header">
+        <span>Oprime el icono y guarda el URL</span>
+        <Container sx={{ flexGrow: 1, justifyContent: "center" }}>
+          {/* Row one */}
+          <Grid container spacing={1} columns={16} justifyContent="center">
+            {/* Facebook */}
+            <Button variant="outlined" onClick={handleClickOpen}>
+              <Grid
+                item
+                xs={8}
+                container
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Item>
+                  <img src={fbicon} alt="icon fb" className="icon" />
+                  <p className="items"> Facebook</p>
+                </Item>
+              </Grid>
+            </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Agregar URL Facebook</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Ingresa el URL de tu red social: (Ej:
+                  https://www.facebook.com/profile.php?id=123456789)
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="url"
+                  label="URL"
+                  type="url"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button onClick={handleClose}>Agregar</Button>
+              </DialogActions>
+            </Dialog>
+            <Button variant="outlined" onClick={handleClickOpen}>
+              {/* Whatsapp */}
+              <Grid
+                item
+                xs={8}
+                container
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Item>
+                  <img src={whaicon} alt="icon wha" className="icon" />
+                  <p className="items"> Whatsapp</p>
+                </Item>
+              </Grid>
+            </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Agregar URL Whatsapp</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Ingresa el URL de tu red social: (Ej:
+                  https://wa.me/1XXXXXXXXXX)
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="url"
+                  label="URL"
+                  type="url"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button onClick={handleClose}>Agregar</Button>
+              </DialogActions>
+            </Dialog>
+          </Grid>
+
+          {/* Row two */}
+          <Grid container spacing={1} columns={16} justifyContent="center">
+            {/* Instagram */}
+            <Button variant="outlined" onClick={handleClickOpen}>
+              <Grid
+                item
+                xs={8}
+                container
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Item>
+                  <img src={instaicon} alt="icon insta" className="icon" />
+                  <p className="items"> Instagram</p>
+                </Item>
+              </Grid>
+            </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Agregar URL Instagram</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Ingresa el URL de tu red social: (Ej:
+                  https://www.facebook.com/profile.php?id=123456789)
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="url"
+                  label="URL"
+                  type="url"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button onClick={handleClose}>Agregar</Button>
+              </DialogActions>
+            </Dialog>
+            <Button variant="outlined" onClick={handleClickOpen}>
+              {/* Twitter */}
+              <Grid
+                item
+                xs={8}
+                container
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Item>
+                  <img src={twiticon} alt="icon twi" className="icon" />
+                  <p className="items"> Twitter</p>
+                </Item>
+              </Grid>
+            </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Agregar URL Twitter</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Ingresa el URL de tu red social: (Ej:
+                  https://wa.me/1XXXXXXXXXX)
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="url"
+                  label="URL"
+                  type="url"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button onClick={handleClose}>Agregar</Button>
+              </DialogActions>
+            </Dialog>
+          </Grid>
+
+          {/* Row three */}
+          <Grid container spacing={1} columns={16} justifyContent="center">
+            {/* Snapchat */}
+            <Button variant="outlined" onClick={handleClickOpen}>
+              <Grid
+                item
+                xs={8}
+                container
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Item>
+                  <img src={snapicon} alt="icon snap" className="icon" />
+                  <p className="items"> Snapchat</p>
+                </Item>
+              </Grid>
+            </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Agregar URL Snapchat</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Ingresa el URL de tu red social: (Ej:
+                  https://www.facebook.com/profile.php?id=123456789)
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="url"
+                  label="URL"
+                  type="url"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button onClick={handleClose}>Agregar</Button>
+              </DialogActions>
+            </Dialog>
+            <Button variant="outlined" onClick={handleClickOpen}>
+              {/* Tiktok */}
+              <Grid
+                item
+                xs={8}
+                container
+                alignItems="center"
+                justifyContent="center"
+              >
+                <Item>
+                  <img src={tikicon} alt="icon tik" className="icon" />
+                  <p className="items"> Tiktok</p>
+                </Item>
+              </Grid>
+            </Button>
+
+            <Dialog open={open} onClose={handleClose}>
+              <DialogTitle>Agregar URL </DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  Ingresa el URL de tu red social: (Ej:
+                  https://wa.me/1XXXXXXXXXX)
+                </DialogContentText>
+                <TextField
+                  autoFocus
+                  margin="dense"
+                  id="url"
+                  label="URL"
+                  type="url"
+                  fullWidth
+                  variant="standard"
+                />
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={handleClose}>Cancelar</Button>
+                <Button onClick={handleClose}>Agregar</Button>
+              </DialogActions>
+            </Dialog>
+          </Grid>
+          <div className="btn-create">
+            <Button
+              onClick={() => {
+                alert("Redes sociales agregadas");
+               
+              }}
+              variant="contained"
+            
+        
+              
+            >
+              Generar codigo QR
+            </Button>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
 
 class Save extends React.Component {
   render() {
     return (
       <>
-      <div className="header">
-        <h3>Oprime el icono y guarda el URL</h3>
-        <Container sx={{ flexGrow: 1 }}>
-          <Grid container spacing={4} columns={16}>
-            <Grid item xs={8}>
-              <Item>
-                {" "}
-                <img src={fbicon} alt="icon fb" className="icon" />
-              </Item>
-            </Grid>
-            <Grid item xs={8}>
-              <Item>
-                {" "}
-                <img src={whaicon} alt="icon wha" className="icon" />
-              </Item>
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={4} columns={16}>
-            <Grid item xs={8}>
-              <Item>
-                {" "}
-                <img src={instaicon} alt="icon wha" className="icon" />
-              </Item>
-            </Grid>
-            <Grid item xs={8}>
-              <Item>
-                {" "}
-                <img src={twiticon} alt="icon wha" className="icon" />
-              </Item>
-            </Grid>
-          </Grid>
-
-          <Grid container spacing={4} columns={16}>
-            <Grid item xs={8}>
-              <Item>
-                {" "}
-                <img src={snapicon} alt="icon wha" className="icon" />
-              </Item>
-            </Grid>
-            <Grid item xs={8}>
-              <Item>
-                {" "}
-                <img src={tikicon} alt="icon tik" className="icon" />
-              </Item>
-            </Grid>
-          </Grid>
-        </Container>
-      </div>
+        <FormDialog />
       </>
     );
   }
